c9d08f39160d0b9cb8172dc9d683ad431e8e1262
Gabriellebase <zhanghua8286@gmail.com>
2017-08-07 19:20:46 +0800

    20170807commit entire solution

---
+++ ConsoleOOD/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOOD
+{
+    public class TestClass
+    {
+        public static int staticMember = 99;
+        public int objectMember = 88;
+
+        public bool GetObjectMember()
+        {
+            Console.WriteLine("获得实例成员："+this.objectMember);
+            Console.WriteLine("获得静态成员：" + TestClass.staticMember);
+            return true;
+        }
+    }
+}
